ded721bb701514bf6a9445f5fdf146f911abf53a
Clara Ameller <claraameller@Claras-MacBook-Pro.local>
2018-01-07 20:19:21 +0100

    Initial commit

--- lib/pometes.rb
+++ lib/pometes.rb
@@ -1,6 +1,17 @@
-class Pometes
-end
-
 ## If you want to see the song in your console uncomment the next line and
 ## execute `ruby lib/pometes.rb`
 # puts Pometes.new.song
+
+class Pometes
+    def verse(verses_count)
+
+    end
+
+    def verses(from, to)
+
+    end
+
+    def song
+
+    end
+end
